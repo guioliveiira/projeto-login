3729996076afde17c2762754b5fed56d36aef9e7
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-03-06 17:46:20 -0300

    first commit

---
+++ src/main/java/com/javacore/spring_api_login/SpringApiLoginApplication.java
@@ -0,0 +1,13 @@
+package com.javacore.spring_api_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiLoginApplication.class, args);
+	}
+
+}
